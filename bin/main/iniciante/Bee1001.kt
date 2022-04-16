8de4e53d1a45dce1c65c67be5433341d0d8878e2
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-16 17:08:34 -0300

    bee 1001 extremamente basico

---
+++ bin/main/iniciante/Bee1001.kt
@@ -0,0 +1,10 @@
+package iniciante
+
+//Extremamente Básico
+
+fun main(args: Array<String>) {
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    val x = a + b
+    println("X = " + x)
+}
