718c68abfd5cd3d242139356426833edd9acea5d
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-16 17:10:12 -0300

    bee 1003 soma simples

---
+++ bin/main/iniciante/Bee1003.kt
@@ -0,0 +1,10 @@
+package iniciante
+
+//Bee1003 - Soma Simples
+
+fun main(){
+    val A = readLine()!!.toInt()
+    val B = readLine()!!.toInt()
+    val soma = A + B
+    println("SOMA = $soma")
+}
