93cebca4fa55159db679ad8a1b8a843ccfb91f70
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-17 22:34:42 -0300

    Bee 1007 finalizado

--- src/main/kotlin/iniciante/Bee1007.kt
+++ src/main/kotlin/iniciante/Bee1007.kt
@@ -2,8 +2,14 @@ package iniciante
 
 //Bee 1007 - Diferença
 
-fun main(){
+fun main(args: Array<String>) {
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    val c = readLine()!!.toInt()
+    val d = readLine()!!.toInt()
 
+    val diferenca = (a * b - c * d)
 
+    println("DIFERENCA = $diferenca")
 
 }
