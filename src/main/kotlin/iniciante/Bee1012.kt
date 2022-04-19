d60a8e8bb7af51b944d8551398f4fb54609daf46
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-19 15:37:16 -0300

    Bee 1012 area - finalizado

--- src/main/kotlin/iniciante/Bee1012.kt
+++ src/main/kotlin/iniciante/Bee1012.kt
@@ -4,7 +4,7 @@ import kotlin.math.pow
 
 fun main(){
 
-    val numeros = readLine()!!.split(" ").map{ it.toDouble() }
+    /*val numeros = readLine()!!.split(" ").map{ it.toDouble() }
 
     val areaTrianguloRetangulo = (numeros[0] * numeros[2])/2
     val areaDoCirculo = 3.14159 * numeros[2].pow(2)
@@ -16,6 +16,23 @@ fun main(){
     println("CIRCULO: %.3f".format(areaDoCirculo))
     println("TRAPEZIO: %.3f".format(areaDoTrapezio))
     println("QUADRADO: %.3f".format(areaDoQuadrado))
-    println("RETANGULO: %.3f".format(areaDoRetangulo))
+    println("RETANGULO: %.3f".format(areaDoRetangulo))*/
+
+ val numeros = readLine()!!.split(" ").map{ it.toDouble() }
+ val a = numeros[0]
+ val b = numeros[1]
+ val c = numeros[2]
+
+ val areaTrianguloRetangulo = (a * c)/2.0
+ val areaDoCirculo = 3.14159 * c.pow(2)
+ val areaDoTrapezio = (a + b)*c/2
+ val areaDoQuadrado = b * b
+ val areaDoRetangulo = a * b
+
+ println("TRIANGULO: %.3f".format(areaTrianguloRetangulo))
+ println("CIRCULO: %.3f".format(areaDoCirculo))
+ println("TRAPEZIO: %.3f".format(areaDoTrapezio))
+ println("QUADRADO: %.3f".format(areaDoQuadrado))
+ println("RETANGULO: %.3f".format(areaDoRetangulo))
 
 }
