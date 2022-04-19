5ce2a5318100b45f1c61129206ba7d7e72abe02d
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-19 09:49:08 -0300

    Wrong answer. Aguardando verificacao

--- src/main/kotlin/iniciante/Bee1012.kt
+++ src/main/kotlin/iniciante/Bee1012.kt
@@ -1,5 +1,21 @@
 package iniciante
 
+import kotlin.math.pow
+
 fun main(){
+
+    val numeros = readLine()!!.split(" ").map{ it.toDouble() }
+
+    val areaTrianguloRetangulo = (numeros[0] * numeros[2])/2
+    val areaDoCirculo = 3.14159 * numeros[2].pow(2)
+    val areaDoTrapezio = ((numeros[0] + numeros[1]) * numeros[2])/2
+    val areaDoQuadrado = numeros[1] * numeros[1]
+    val areaDoRetangulo = numeros[0] * numeros[1]
+
+    println("TRIANGULO: %.3f".format(areaTrianguloRetangulo))
+    println("CIRCULO: %.3f".format(areaDoCirculo))
+    println("TRAPEZIO: %.3f".format(areaDoTrapezio))
+    println("QUADRADO: %.3f".format(areaDoQuadrado))
+    println("RETANGULO: %.3f".format(areaDoRetangulo))
 
 }
