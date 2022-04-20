8ecc4652d7f8c84b1ac76295c69b91cf97e305fe
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-20 13:47:50 -0300

    Bee 1015 passou

---
+++ src/main/kotlin/iniciante/Bee1015.kt
@@ -0,0 +1,18 @@
+package iniciante
+
+import kotlin.math.pow
+
+fun main(){
+
+    val p1 = readLine()!!.split(" ").map{ it.toDouble() }
+    val p2 = readLine()!!.split(" ").map{ it.toDouble() }
+
+    val x1 = p1[0]
+    val y1 = p1[1]
+    val x2 = p2[0]
+    val y2 = p2[1]
+
+    val calculo = Math.sqrt(((x2 - x1).pow(2)) + ((y2 - y1).pow(2)))
+
+    println("%.4f".format(calculo))
+}
