7f2a2d463a55f96d19abadc4e6ab01c90459077e
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-24 12:37:51 -0300

    Bee 1020 idade em dias aceito

---
+++ src/main/kotlin/iniciante/Bee1020.kt
@@ -0,0 +1,23 @@
+package iniciante
+
+import kotlin.math.roundToInt
+
+fun main(){
+    //Considerar ano com 365 dias
+    //Considerar mês comn 30 dias
+
+    var idadeEmDias = readLine()!!.toInt()
+    var meses = 0
+    var anos = 0
+
+
+    anos = idadeEmDias / 365
+    println("$anos ano(s)")
+    idadeEmDias = idadeEmDias - (anos * 365)
+    meses = idadeEmDias / 30
+    println("$meses mes(es)")
+    idadeEmDias = idadeEmDias - (meses * 30)
+    println("$idadeEmDias dia(s)")
+
+
+}
