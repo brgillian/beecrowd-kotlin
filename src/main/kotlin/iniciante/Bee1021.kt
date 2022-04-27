dfbffd5199f4fa7f812f9a2448ebd975c0123535
Gillian Gomes <38887237+brgillian@users.noreply.github.com>
2022-04-26 21:25:36 -0300

    Excluindo rascunho e colocando a resposta final que foi aprovada.

--- src/main/kotlin/iniciante/Bee1021.kt
+++ src/main/kotlin/iniciante/Bee1021.kt
@@ -10,47 +10,62 @@ import javax.swing.text.NumberFormatter
 //Notas: 100, 50, 20, 10, 5, 2
 //Modedas: 1, 0.50, 0.25, 0.10, 0.05 e 0.01
 
-/*fun main(){
+fun main(){
     var input = readLine()!!.toDouble()
     var qtdDinheiro = 0
+
     println("NOTAS:")
-    qtdDinheiro = (input/100).toInt()
+    qtdDinheiro =  input.toInt() / 100
     println("$qtdDinheiro nota(s) de R$ 100.00")
-    input = input % 100
-    qtdDinheiro = (input / 50.00).toInt()
+    input = input % 100.0
+
+    qtdDinheiro =  input.toInt() / 50
     println("$qtdDinheiro nota(s) de R$ 50.00")
-    input = input % 50.00
-    qtdDinheiro = (input / 20.00).toInt()
+    input = input % 50.0
+
+    qtdDinheiro =  input.toInt() / 20
     println("$qtdDinheiro nota(s) de R$ 20.00")
-    input = input % 20.00
-    qtdDinheiro = (input / 10.00).toInt()
+    input = input % 20.0
+
+    qtdDinheiro =  input.toInt() / 10
     println("$qtdDinheiro nota(s) de R$ 10.00")
-    input = input % 10.00
-    qtdDinheiro = (input / 5.00).toInt()
+    input = input % 10.0
+
+    qtdDinheiro =  input.toInt() / 5
     println("$qtdDinheiro nota(s) de R$ 5.00")
-    input = input % 5.00
-    qtdDinheiro = (input / 2.00).toInt()
+    input = input % 5.0
+
+    qtdDinheiro =  input.toInt() / 2
     println("$qtdDinheiro nota(s) de R$ 2.00")
-    input = input % 2.00
+    input = input % 2.0
+
+    input = input * 100.0
     println("MOEDAS:")
-    qtdDinheiro = (input / 1.00).toInt()
+
+    qtdDinheiro =  input.toInt() / 100
     println("$qtdDinheiro moeda(s) de R$ 1.00")
-    input = input % 1.00
-    qtdDinheiro = (input / 0.50).toInt()
+    input = input % 100.0
+
+    qtdDinheiro =  input.toInt() / 50
     println("$qtdDinheiro moeda(s) de R$ 0.50")
-    input = input % 0.50
-    qtdDinheiro = (input / 0.25).toInt()
+    input = input % 50.0
+
+    qtdDinheiro =  input.toInt() / 25
     println("$qtdDinheiro moeda(s) de R$ 0.25")
-    input = input %  0.25
-    qtdDinheiro = (input / 0.10).toInt()
+    input = input % 25.0
+
+    qtdDinheiro =  input.toInt() / 10
     println("$qtdDinheiro moeda(s) de R$ 0.10")
-    input = input % 0.10
-    qtdDinheiro =(input / 0.05).toInt()
-    println("%.2f moeda(s) de R$ 0.05")
-    input = input % 0.05
-    qtdDinheiro = (input / 0.01).toInt()
+    input = input % 10.0
+
+    qtdDinheiro =  input.toInt() / 5
+    println("$qtdDinheiro moeda(s) de R$ 0.05")
+    input = input % 5.0
+
+    qtdDinheiro =  input.toInt() / 1
     println("$qtdDinheiro moeda(s) de R$ 0.01")
+    input = input % 1.0
  }
- */
+
 
 
